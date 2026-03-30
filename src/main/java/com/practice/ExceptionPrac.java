a85c9ec832bb922a33bbc712065e6a145e840cf0
S0umyajit <soumyajit2k18@gmail.com>
2026-03-31 00:35:27 +0530

    Exception Hanlde Practice

---
+++ src/main/java/com/practice/ExceptionPrac.java
@@ -0,0 +1,18 @@
+package com.practice;
+
+public class ExceptionPrac {
+    public static void main(String[] args) {
+        int a=10,b=0,c;
+        try {
+            c = a / b;
+            System.out.println(c);
+        }catch (ArithmeticException ae){
+            ae.printStackTrace();
+//            System.out.println(ae);
+//            System.out.println(ae.getMessage());
+//            System.out.println(ae.toString());
+        }
+        System.out.println("hello");
+
+    }
+}
