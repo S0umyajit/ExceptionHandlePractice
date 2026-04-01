6dc0d4f0623bb1c83450af3dc99f4247439d0c17
S0umyajit <soumyajit2k18@gmail.com>
2026-04-02 02:39:55 +0530

    custom exception and uses of throw keyword

---
+++ src/main/java/com/practice/customException/Voting.java
@@ -0,0 +1,22 @@
+package com.practice.customException;
+
+import java.util.Scanner;
+//creating custom exception
+class YoungAgeException extends RuntimeException{
+    YoungAgeException(String msg){
+        super(msg);
+    }
+}
+public class Voting {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age=sc.nextInt();
+        if(age<18){
+            throw new YoungAgeException("You are too young to vote");
+        }
+        else {
+            System.out.println("You can vote");
+        }
+    }
+}
